765dbdd5d7d73acef1a869c3cbec59bf5b89d10b
kroekchai12 <118949731+kroekchai12@users.noreply.github.com>
2023-03-02 10:39:20 +0700

    .

--- Navigation/RootStack.js
+++ Navigation/RootStack.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import BottomTab from "./BottomTab";
 import PokemonTab from "./PokemonTab";
@@ -11,16 +11,15 @@ import LocationQuiz from "../screens/week11/LocationQuiz";
 const Stack = createStackNavigator();
 
 export default function RootStack() {
-    return (
-        <Stack.Navigator initialRouteName="BottomTab">
-            <Stack.Screen name="BottomTab" component={BottomTab} options={{ headerShown: false }} />
-            <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title: "Pokemon World" }} />
-            <Stack.Screen name="Book" component={Book} options={{ title: "Book" }} />
-            <Stack.Screen name="BookDetail" component={BookDetail} options={{ title: "Book Detail" }} />
-            <Stack.Screen name="BookForm" component={BookForm} />
-
-            <Stack.Screen name="Location" component={Location} />
-            <Stack.Screen name="LocationQuiz" component={LocationQuiz} />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator initialRouteName="BottomTab">
+      <Stack.Screen name="BottomTab" component={BottomTab} options={{ headerShown: false }} />
+      <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title : "Pokemon World" }}  />
+      <Stack.Screen name="Book" component={Book} options={{ title: "Book" }} />
+      <Stack.Screen name="BookDetail" component={BookDetail} options={{ title: "Book Detail" }} />
+      <Stack.Screen name="BookForm" component={BookForm}   />
+      <Stack.Screen name="Location" component={Location} />
+      <Stack.Screen name="LocationQuiz" component={LocationQuiz} />
+    </Stack.Navigator>
+  );
 }
